f9cf925f82f6cadd341bc076b65cbea69f9dd6be
liuqinh2s <liuqinh2s@gmail.com>
2019-06-14 14:17:21 +0800

    update

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="comment-main">
     <div>
-      <form class="new-comment">
+      <div class="new-comment">
         <a>
           <img class="comment-avatar"
                src="//upload.jianshu.io/users/upload_avatars/3089421/8bb9554e-e13c-4ee0-b20f-2ee42474a23d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/114/h/114/format/webp">
         </a>
         <textarea placeholder="写下你的评论..." v-model="commentContent" class="comment-area"></textarea>
-      </form>
+      </div>
       <div class="btns">
-<!--        <span class="btn-cancel" @click="cancelSend">取消</span>-->
+        <!--        <span class="btn-cancel" @click="cancelSend">取消</span>-->
         <button class="btn-send" @click="sendComment(commentContent)">发送</button>
       </div>
     </div>
     <ul class="comments">
-      <li v-for="(item,index) in items">
+      <li v-for="(item,index) in items" class="top-comment">
         <div class="comment-title">
           <img :src="item.avatarUrl" class="comment-avatar">
           <div class="comment-info">
@@ -28,21 +28,48 @@
             <div :class="item.ifZaned>0?'thumb thumb-active':'thumb'">
               <i class="far fa-thumbs-up"></i>
             </div>
-            <span v-if="item.zanNum===0"> 赞</span>
-            <span v-else>{{item.zanNum}} 赞</span>
+            <span v-if="item.zanNum===0" class="comment-meta-span"> 赞</span>
+            <span v-else class="comment-meta-span">{{item.zanNum}} 赞</span>
           </div>
           <div class="reply" @click="reply(index)">
             <i class="far fa-comment-alt"></i>
-            <span>回复</span>
+            <span class="comment-meta-span">回复</span>
           </div>
+          <span v-if="$store.state.userId===item.userId" @click="deleteComment(item.commentId)" class="remove-comment">
+              删除
+            </span>
         </div>
-        <form v-show="showReply[index]" class="add-comment">
+        <div v-if="showReply===index" class="add-comment">
           <textarea placeholder="写下你的评论..." v-model="commentContent1" class="comment-area"></textarea>
           <div class="btns">
             <span class="btn-cancel" @click="cancelSend(index)">取消</span>
-            <button class="btn-send" @click="sendComment(commentContent1)">发送</button>
+            <button class="btn-send" @click="sendSubComment(commentContent1, item.commentId)">发送</button>
           </div>
-        </form>
+        </div>
+        <ul class="sub-comment" v-if="item.subComments.length>0">
+          <li v-for="(subComment,index1) in item.subComments">
+            <span class="user-link">{{subComment.name}}: </span>
+            <span class="user-link" v-if="subComment.atUserName">@{{subComment.atUserName}} </span>
+            <pre class="content">{{subComment.content}}</pre>
+            <span class="time">{{timeStamp2Time(subComment.createTime)}}</span>
+            <div class="reply comment-meta little" @click="replySub(index1)">
+              <i class="far fa-comment-alt"></i>
+              <span>回复</span>
+            </div>
+            <span v-if="$store.state.userId===subComment.userId" @click="deleteComment(subComment.commentId)" class="remove-comment">
+              删除
+            </span>
+            <div v-if="showSubReply===index1" class="add-comment">
+              <textarea placeholder="写下你的评论..." v-model="subCommentContent" class="comment-area"></textarea>
+              <div class="btns">
+                <span class="btn-cancel" @click="cancelSend(index1)">取消</span>
+                <button class="btn-send"
+                        @click="sendSubCommentAt(subCommentContent, item.commentId, subComment.userId)">发送
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -58,7 +85,9 @@
         items: [],
         commentContent: "",
         commentContent1: "",
-        showReply: []
+        showReply: -1,
+        showSubReply: -1,
+        subCommentContent: ""
       }
     },
     props: ['articleId'],
@@ -66,8 +95,8 @@
       timeStamp2Time(time) {
         return timeStamp2Time(time)
       },
-      cancelSend(index){
-        this.showReply.splice(index,1,false)
+      cancelSend(index) {
+        this.showReply = -1
         this.commentContent1 = ""
       },
       sendComment(content) {
@@ -85,6 +114,47 @@
               console.log(res)
               that.items.splice(0, that.items.length)
               that.items = res.data
+            })
+          }
+        })
+      },
+      deleteComment(commentId){
+        let data={
+          userId: this.$store.state.userId,
+          commentId: commentId
+        }
+        this.$http.post("http://192.168.1.151:8080/api/v1/comment/removeComment", data).then((res) => {
+          console.log(res)
+          if(res.data.code===200){
+            this.$http.get("http://192.168.1.151:8080/api/v1/article/getComments?articleId=" + this.articleId).then((res) => {
+              console.log(res)
+              this.items.splice(0, this.items.length)
+              this.items = res.data
+            })
+          }
+        })
+      },
+      sendSubComment(content, commentId) {
+        this.sendSubCommentAt(content, commentId, "")
+      },
+      sendSubCommentAt(content, commentId, atUserId) {
+        let data = {
+          commentId: commentId,
+          userId: this.$store.state.userId,
+          content: content,
+          atUserId: atUserId
+        }
+        this.showReply = -1
+        this.showSubReply = -1
+        this.subCommentContent = ""
+        this.$http.post("http://192.168.1.151:8080/api/v1/comment/addComment", data).then((res) => {
+          console.log(res)
+          if (res.data.code === 200) {
+            this.commentContent = ""
+            this.$http.get("http://192.168.1.151:8080/api/v1/article/getComments?articleId=" + this.articleId).then((res) => {
+              console.log(res)
+              this.items.splice(0, this.items.length)
+              this.items = res.data
             })
           }
         })
@@ -114,7 +184,20 @@
         })
       },
       reply(index) {
-        this.showReply.splice(index,1,!this.showReply[index])
+        this.commentContent1 = ""
+        if (this.showReply === index) {
+          this.showReply = -1
+        } else {
+          this.showReply = index
+        }
+      },
+      replySub(index) {
+        this.subCommentContent = ""
+        if (this.showSubReply === index) {
+          this.showSubReply = -1
+        } else {
+          this.showSubReply = index
+        }
       }
     },
     mounted() {
@@ -172,7 +255,7 @@
     /*display: block;*/
   }
 
-  .comment-main li {
+  .comment-main .comments .top-comment {
     list-style-type: none;
     margin: 0;
     padding: 1rem 0 1rem;
@@ -191,7 +274,6 @@
   .comment-title {
     display: flex;
     align-items: center;
-    padding-bottom: 1rem;
   }
 
   .user-name {
@@ -202,17 +284,18 @@
   .time {
     font-size: 12px;
     color: #969696;
+    margin-right: 1rem;
   }
 
   .content {
-    padding-bottom: 1rem;
+    padding: 1rem 0;
   }
 
   .comment-meta {
     color: #969696;
   }
 
-  .comment-meta span {
+  .comment-meta-span {
     margin-right: 1rem;
     font-size: 14px;
   }
@@ -237,25 +320,60 @@
     color: #01a0e4;
   }
 
-  .add-comment{
+  .add-comment {
     /*padding: 1rem 0;*/
-    height: 160px;
+    height: 130px;
     margin-top: 20px;
     padding: 5px 0 5px 20px;
     border-left: 2px solid #d9d9d9;
   }
 
-  .comment-main .btns{
+  .comment-main .btns {
     float: right;
   }
 
-  .comment-main .btn-cancel{
+  .comment-main .btn-cancel {
     margin-right: 1rem;
     color: #969696;
   }
 
-  .comment-main .btn-cancel:hover{
+  .comment-main .btn-cancel:hover {
     color: black;
     cursor: pointer;
+  }
+
+  .user-link {
+    color: #3194d0;
+  }
+
+  .user-link:hover {
+    cursor: pointer;
+  }
+
+  .sub-comment {
+    margin-top: 20px;
+    padding: 5px 0 5px 20px;
+    border-left: 2px solid #d9d9d9;
+  }
+
+  .sub-comment li {
+    list-style-type: none;
+  }
+
+  .little {
+    font-size: 12px;
+    height: 18px;
+  }
+
+  .sub-comment li {
+    padding: 0.5rem 0;
+    border-bottom: 1px solid #f0f0f0;
+  }
+
+  .remove-comment{
+    font-size: 14px;
+    float: right;
+    cursor: pointer;
+    color: #969696;
   }
 </style>
